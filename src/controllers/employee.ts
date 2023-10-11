cccfbcf653a296ffb5c2526cb01723e34ded6199
Luis Sucuc <jlsucuc@gmail.com>
2023-10-10 21:12:03 -0600

    feat: Improve routes and controllers

--- src/controllers/employee.ts
+++ src/controllers/employee.ts
@@ -1,22 +1,19 @@
 import axios from 'axios'
 import { Request, Response } from 'express'
-import status from 'http-status-codes'
 import { EMPLOYEES_API } from '../constants/apis'
-import { EmployeeResponse } from '../types/employee'
+import { Employee, EmployeeResponse } from '../types/employee'
 
-const getAll = async (req: Request, res: Response) => {
+const getAll = async (): Promise<Employee[]> => {
   try {
     const { data }: { data: EmployeeResponse } = await axios.get(
       `${EMPLOYEES_API}/employees`
     )
 
-    res.status(status.OK).send(data.data)
+    return data.data
   } catch (error: unknown) {
     // Log error and send response
     console.log(error)
-    res
-      .status(status.INTERNAL_SERVER_ERROR)
-      .send({ description: (error as Error).message })
+    throw new Error((error as Error).message)
   }
 }
 
--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -1,5 +1,25 @@
+import { Request, Response } from 'express'
 import express from 'express'
-export const employee = express.Router()
+import status from 'http-status-codes'
 import { employeeController } from '../controllers/employee'
+import { Employee } from '../types/employee'
 
-employee.get('/', employeeController.getAll)
+export const employee = express.Router()
+
+employee.get(
+  '/',
+
+  async (req: Request, res: Response) => {
+    try {
+      const controllerResponse: Employee[] = await employeeController.getAll()
+
+      res.status(status.OK).send(controllerResponse)
+    } catch (error: unknown) {
+      // Log error and send response
+      console.log(error)
+      res
+        .status(status.INTERNAL_SERVER_ERROR)
+        .send({ description: (error as Error).message })
+    }
+  }
+)
