0f58d9a9cf2210f40a08d8061ce3c88ea3217be6
Luis Sucuc <jlsucuc@gmail.com>
2023-10-12 16:24:01 -0600

    feat: Add cart_percentage mockup

--- mockups.py
+++ mockups.py
@@ -3,3 +3,4 @@ import utils
 # Import the products from the JSON file
 products = utils.open_json_file('products.json')
 cart = utils.open_json_file('cart.json')
+cart_percentage = utils.open_json_file('cart_percentage.json')
